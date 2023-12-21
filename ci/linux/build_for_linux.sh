2f457b7685b7cd1e904ba5c0dc01d90676cb0af8
Jyothish Kumar M S <jeevanms15@gmail.com>
2023-12-21 15:05:37 +0530

    Update build_for_linux.sh

--- ci/linux/build_for_linux.sh
+++ ci/linux/build_for_linux.sh
@@ -68,13 +68,14 @@ if [ $? -ne 0 ] ;then
     exit 1
 fi
 
-cmake --build . --config "$build_mode" -j8 \
 CC="$CC"                        \
 CXX="$CXX"                      \
 LDFLAGS="-L$MUSL_LIB -Wl,-rpath,$MUSL_LIB"      \
 CFLAGS="-I$MUSL_INC"                    \
 CXXFLAGS="-I$MUSL_INC"                  \
-CPPFLAGS="-I$MUSL_INC"
+CPPFLAGS="-I$MUSL_INC" \
+cmake --build . --config "$build_mode" -j8
+
 
 if [ $? -ne 0 ] ;then
     echo "error: CMake build failed, exiting......"
